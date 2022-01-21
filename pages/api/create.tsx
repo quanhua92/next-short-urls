87e291d94b3dad7482796373fbe7c25d1c05b012
quanhua92 <quanhua92@gmail.com>
2022-01-21 11:24:58 +0700

    generate link with retry

--- pages/api/create.tsx
+++ pages/api/create.tsx
@@ -1,9 +1,52 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { randomBytes } from "crypto";
 import { prisma } from "../../lib/prisma";
+import { Link } from "prisma/prisma-client";
 
 type Data = {
   shortUrl?: string;
   message: string;
+};
+
+const GENERATED_SHORT_URL_LENGTH = 6;
+const MAX_RETRY = 5;
+
+const createLinkWithRetry = async function (
+  url: string,
+  shortUrl: string,
+  random_length: number,
+  retry: number
+): Promise<Link | undefined> {
+  var expectedShortUrl = shortUrl as string;
+
+  for (let i = 0; i < retry; i++) {
+    if (expectedShortUrl.length == 0) {
+      const result = randomBytes(random_length / 2 + 1).toString("hex");
+      expectedShortUrl = result.substring(0, random_length);
+    }
+
+    const existedLink = await prisma.link.findFirst({
+      select: {
+        shortUrl: true,
+      },
+      where: {
+        shortUrl: expectedShortUrl,
+      },
+    });
+
+    if (existedLink !== null) {
+      expectedShortUrl = "";
+      continue;
+    }
+
+    const link = await prisma.link.create({
+      data: {
+        url: url,
+        shortUrl: expectedShortUrl,
+      },
+    });
+    return link;
+  }
 };
 
 export default async function handler(
@@ -11,19 +54,24 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   const { url, shortUrl } = req.body;
+
   try {
-    // TODO: check if shortURL is valid or empty -> generate
-
-    const link = await prisma.link.create({
-      data: {
-        url: url,
+    const link = await createLinkWithRetry(
+      url,
+      shortUrl,
+      GENERATED_SHORT_URL_LENGTH,
+      MAX_RETRY
+    );
+    if (link === undefined) {
+      res.status(400).json({
         shortUrl: shortUrl,
-      },
-    });
-
+        message: `Invalid short url: / ${shortUrl}`,
+      });
+      return;
+    }
     res.status(200).json({
       shortUrl: shortUrl,
-      message: `Created successfully: / ${shortUrl}`,
+      message: `Created successfully: / ${link.shortUrl}`,
     });
   } catch (error) {
     console.log(error);
