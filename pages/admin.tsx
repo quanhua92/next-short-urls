7646c61d14bf9164867ae3b1af7c6b9ec7f78f1e
quanhua92 <quanhua92@gmail.com>
2022-01-22 14:18:58 +0700

    admin page with table

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -15,22 +15,88 @@ export default function Admin() {
   }
 
   return (
-    <div className="flex flex-col h-screen max-w-7xl items-center justify-center m-auto">
+    <div className="flex flex-col h-screen max-w-7xl mx-auto">
       Admin Page -{" "}
       <Link href="/">
         <a>Home</a>
       </Link>
-      <ul>
-        {data &&
-          data.data?.map((link, i) => {
-            return (
-              <li key={i}>
-                {link.url} | <a href={link.shortUrl}>{link.alias}</a> |{" "}
-                {link.clicks}
-              </li>
-            );
-          })}
-      </ul>
+      <div className="flex flex-col">
+        <div className="overflow-x-scroll">
+          <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Alias
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Short URL
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Original URL
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Clicks
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Edit
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data &&
+                    data.data?.map((link, i) => {
+                      return (
+                        <tr
+                          key={i}
+                          className={i % 2 === 0 ? "bg-white" : "bg-gray-50"}
+                        >
+                          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                            {link.alias}
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                            {link.shortUrl}
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                            {link.url}
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                            {link.clicks}
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                            <a
+                              href="#"
+                              className="text-blue-600 hover:text-blue-900"
+                            >
+                              Edit
+                            </a>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <ul></ul>
     </div>
   );
 }
