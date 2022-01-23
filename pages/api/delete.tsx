07021509d1d14c4c1eb0e8d5122c2781f9b47456
quanhua92 <quanhua92@gmail.com>
2022-01-23 08:03:34 +0700

    delete api

---
+++ pages/api/delete.tsx
@@ -0,0 +1,50 @@
+import { Link, User } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { withIronSessionApiRoute } from "iron-session/next";
+import { prisma } from "../../lib/prisma";
+import { sessionOptions } from "../../lib/session";
+import { Role } from "@prisma/client";
+import { getUserById } from "../../lib/user";
+
+export type Data = {
+  status: boolean;
+  message?: string;
+};
+
+export default withIronSessionApiRoute(handler, sessionOptions);
+
+async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const { alias } = req.body;
+  const userSession = req.session.user;
+  if (!userSession) {
+    res.status(400).json({ status: false, message: "Unauthorized" });
+    return;
+  }
+
+  const user = await getUserById(userSession.id);
+  if (!user) {
+    res.status(400).json({ status: false, message: "Unauthorized" });
+    return;
+  }
+
+  var condition: { alias: string; userId?: string } = {
+    alias: alias as string,
+  };
+  if (user.role !== Role.ADMIN) {
+    condition = {
+      alias: alias as string,
+      userId: user.id,
+    };
+  }
+
+  const link = await prisma.link.delete({
+    where: condition,
+  });
+
+  if (link === null) {
+    res.status(400).json({ status: false, message: "Invalid link" });
+    return;
+  }
+
+  res.status(200).json({ status: true });
+}
